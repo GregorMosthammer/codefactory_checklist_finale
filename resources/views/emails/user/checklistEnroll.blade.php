cbb5cc877426e1f5821ec158d5279feb9672bcab
[gregormosthammer] <[g.mosthammer@gmail.com]>
2018-03-14 21:01:01 +0100

    email changes

--- resources/views/emails/user/checklistEnroll.blade.php
+++ resources/views/emails/user/checklistEnroll.blade.php
@@ -1,21 +1,21 @@
 @component('mail::message')
-Thank you for your application and Welcome to the CodeFactory!
+Thank you for your application and <b>Welcome to the CodeFactory! </b>
 We are very happy that you decided to take this step with us and are looking forward to 15 intensive and amazing weeks with you.
-In order for you to enjoy and benefit from your time with us the most, here are some informations on the course of events, preparations and miscellaneous information.
+In order for you to enjoy and benefit from your time with us the most, here are some <b> informations on the course of events, preparations and miscellaneous information.</b>
 
 A CodeFactory Representative will contact you shortly via the provided phone number, to go over the schedule and all other details together with you.
  
-To pay for the Fullstack-course, please wire the program fee of  18.500$ to our account at your earliest convenience:
+To pay for the Fullstack-course, please wire the program fee of  18.500$ to <b>  our account </b> at your earliest convenience:
  
-Erste Bank<br>
-Kontoinhaber: CodeFactory GmbH<br>
-Adresse: Herklotzgasse 21, 1150 Wien<br>
-IBAN: AT412011182934111100<br>
-BIC/SWIFT GIBAATWWXXX<br>
+<b> Erste Bank</b><br>
+<b> Kontoinhaber: CodeFactory GmbH</b><br>
+<b> Adresse: Herklotzgasse 21, 1150 Wien</b><br>
+<b> IBAN: AT412011182934111100</b><br>
+<B> BIC/SWIFT GIBAATWWXXX</b><br>
 
  
-The Fullstack-course spans 15 weeks of full-time work, which means from 9 in the morning to 4 pm. Monday through Thursday, the training will occur in our office, attended by two trainers.
-Unless otherwise proclaimed, the course will happen at our office located at Kettenbrückengasse 21 / 2 / 12, 1050 Wien. Please bring your own Laptop, where our trainers will explain everything else to you on day 1.
+The <b> Fullstack-course </b> spans <b> 15 weeks of full-time work, </b> which means from <b> 9 in the morning to 4 pm. </b> Monday through Thursday, the training will occur in our office, attended by two trainers.
+Unless otherwise proclaimed, the course will happen at <b> our office located at Kettenbrückengasse 21 / 2 / 12, 1050 Wien. </b> Please <b> bring your own Laptop, </b> where our trainers will explain everything else to you on day 1.
 On most Fridays, there will be a so-called “Code-review”, where the skills you acquired over the week will be realized in a small-scale project. This will be reviewed by our trainers over the weekend and you will receive feedback before training starts again on the next Monday.
 
 You can also plan your own break-schedule, but our experience shows that most of your colleagues will take a regular lunch break. This is a great chance to connect and relax with them!
@@ -23,17 +23,17 @@ You can also plan your own break-schedule, but our experience shows that most of
 Our office is equipped with everything needed for a productive and comfortable day of work and studying. A comfy corner-sofa, kitchen with a fridge, microwave and coffeemaker as well as large, open tables for teamwork.
 You are also invited to stay after the official end of your workday, if you wish to relax or collaborate further with your colleagues.
  
-In general, you will work with pair-programming during your course. This means, that you will closely collaborate with one other student to create your classwork examples and projects. Through this, you will not only get to know your fellow students quickly, but you will also work with higher concentration and learn to communicate in programming terms very easily.
+<b> In general, you will work with pair-programming during your course. </b> This means, that you will closely collaborate with one other student to create your classwork examples and projects. Through this, you will not only get to know your fellow students quickly, but you will also work with higher concentration and learn to communicate in programming terms very easily.
  
-The time you spend in the course is dedicated fully to realizing your projects and tasks. The little theory that is necessary to start your work, you should read over the day before or over the weekend on your own. We promise however, that you will be able to do so quite easily ;)
+The time you spend in the course is dedicated fully to realizing your projects and tasks. <b> The little theory that is necessary to start your work, you should read over the day before or over the weekend on your own. </b> We promise however, that you will be able to do so quite easily ;)
 During the course, you will also work on projects and examples from the “real world” (f.e. on a Homepage for a local NGO), through which you will, by the end of the course, have an impressive portfolio with which you can easily demonstrate your new skills when applying for a job.
  
-Our online-curriculum will be available for you from the start of the course at the url lms.codefactory.wien, and you can read over every part of it at your convenience.
+<b>Our online-curriculum will be available for you from the start of the course at the url lms.codefactory.wien </b> , and you can read over every part of it at your convenience.
 You will receive the login-credentials via email a week before the classes start.
 
-For the Fullstack-course you are required to know the basics of HTML/CSS and JavaScript, either through self-study or through our basic courses for HTML / CSS and JavaScript, which we highly recommend you take as a first step into web development.
+<b> For the Fullstack-course you are required to know the basics of HTML/CSS and JavaScript, </b> either through self-study or through our basic courses for HTML / CSS and JavaScript, which we highly recommend you take as a first step into web development.
 
-If you have further questions about the course, curriculum, course of events or other things, just writes us an eMail or call just call us, we will be happy to assist you :)
+<b> If you have further questions about the course, curriculum, course of events or other things, just writes us an eMail or call just call us, <b> we will be happy to assist you :)
 
 We are looking forward to having you in our cohort! Happy Coding!
 
